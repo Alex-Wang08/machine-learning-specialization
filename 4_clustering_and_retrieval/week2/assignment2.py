48ad493df8c659efe1f6bb30d11b0ad6053666a7
alexwang <xiaojie.wang115@gmail.com>
2021-07-14 01:03:00 -0600

    add assignment2

---
+++ 4_clustering_and_retrieval/week2/assignment2.py
@@ -0,0 +1,12 @@
+"""locality sensitive hashing"""
+import numpy as np
+import turicreate
+from scipy.sparse import csr_matrix
+from sklearn.metrics.pairwise import paired_distances
+from copy import copy
+import matplotlib.pyplot as plt
+
+wiki = turicreate.SFrame('people_wiki.sframe')
+wiki = wiki.add_row_number()
+
+wiki['tf_idf'] = turicreate.text_analytics.tf_idf(wiki['text'])
