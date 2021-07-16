72c9bfed6009dd8da6ad166386735d8a585e7adb
alexwang <xiaojie.wang115@gmail.com>
2021-07-16 00:16:03 -0600

    add assignment3

--- 4_clustering_and_retrieval/week2/assignment2.py
+++ 4_clustering_and_retrieval/week2/assignment2.py
@@ -10,3 +10,9 @@ wiki = turicreate.SFrame('people_wiki.sframe')
 wiki = wiki.add_row_number()
 
 wiki['tf_idf'] = turicreate.text_analytics.tf_idf(wiki['text'])
+
+
+def sframe_to_scipy(column):
+    x = turicreate.SFrame({'X1': column})
+    x = x.add_row_number()
+    x = x.stack()
